0f5962c265eb223d8549a52c9e78816f00c48281
Jonathan East <jonathan_east@ymail.com>
2020-07-14 21:03:54 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.all.artists.name
+    self.all.each do |name|
+      artists.name
+    end
   end
 end
